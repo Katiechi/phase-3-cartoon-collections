c331971a383f42466289691911b0cf19212e80b0
brian ambundo <brian.katiechi9@gmail.com>
2022-08-19 10:50:49 +0300

    commit tree be0f6ff8154dbf66cf0c451b48b1e51d5126e28c

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,41 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves (dwarves)# code an argument here
   # Your code here
+  dwarves.each_with_index.map do |name, idx|
+    puts "#{idx+1}. #{name}"
+  end
 end
+puts roll_call_dwarves(["Doc", "Dopey", "Bashful", "Grumpy"])
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet (array)# code an argument here
   # Your code here
+  new_array = array.join("! ")
+  capitalized=new_array.split.each{|i| i.capitalize!}.join(' ')
+  new_capitalized=capitalized << "!"
+  new_capitalized.split(" ")
 end
+puts summon_captain_planet(planeteer_calls = ["earth", "wind", "fire", "water", "heart"])
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls (array)# code an argument here
   # Your code here
+  long_word = array.filter do |word|
+    word.length >4
+  end
+  puts long_word
+  if long_word.empty? == true
+    long_word == "string"
+  else
+    long_word == long_word
+  end
+  
 end
+puts long_planeteer_calls(["two", "go", "industrious","bop"])
 
-def find_the_cheese# code an argument here
+
+def find_the_cheese(array)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  cheese=array.find do |word|
+    found_cheese = cheese_types.include?word
+  end
 end
+puts find_the_cheese(["tomato soup", "cheddar", "oyster crackers", "gouda"])
